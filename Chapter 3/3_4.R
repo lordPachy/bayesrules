ca609c84d27d870d75d68f56cfcd0e92b28d484a
lordPachy <francescocalzona03@gmail.com>
2025-07-11 14:58:24 +0200

    Chapter 3 exercises

---
+++ Chapter 3/3_4.R
@@ -0,0 +1,5 @@
+# Imports
+library('bayesrules')
+library('tidyverse')
+
+plot_beta(0.5, 6)
---
+++ Chapter 3/3_6.R
@@ -0,0 +1,11 @@
+# Imports
+library('bayesrules')
+library('tidyverse')
+
+# Summarizing properties of beta distr.
+summarize_beta(0.5, 0.5)
+summarize_beta(1, 1)
+summarize_beta(2, 2)
+summarize_beta(6, 6)
+summarize_beta(6, 2)
+summarize_beta(0.5, 6)
